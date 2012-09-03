9efac50a6aafadebd15d06cebff5d75251768b96
eller86 <skypencil@gmail.com>
2012-09-03 22:43:10 +0800

    added test case to test OpcodeStack which has an Exception

--- bug-pattern/src/test/java/jp/skypencil/findbugs/slf4j/WrongPlaceholderDetectorTest.java
+++ bug-pattern/src/test/java/jp/skypencil/findbugs/slf4j/WrongPlaceholderDetectorTest.java
@@ -1,9 +1,14 @@
 package jp.skypencil.findbugs.slf4j;
 
-import static org.junit.Assert.*;
-
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
 import org.junit.Test;
+
+import edu.umd.cs.findbugs.OpcodeStack;
+import edu.umd.cs.findbugs.OpcodeStack.Item;
 
 public class WrongPlaceholderDetectorTest {
 	@Test
@@ -29,10 +34,23 @@ public class WrongPlaceholderDetectorTest {
 	@Test
 	public void testCountParameterWithoutArray() {
 		WrongPlaceholderDetector detector = new WrongPlaceholderDetector(null);
-		assertThat(detector.countParameter(null, "(Ljava/lang/String;)V"), is(0));
-		assertThat(detector.countParameter(null, "(Ljava/lang/String;Ljava/lang/Object;)V"), is(1));
-		assertThat(detector.countParameter(null, "(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"), is(2));
-		assertThat(detector.countParameter(null, "(Lorg/slf4j/Maker;Ljava/lang/String;Ljava/lang/Object;)V"), is(1));
-		assertThat(detector.countParameter(null, "(Lorg/slf4j/Maker;Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"), is(2));
+		OpcodeStack stack = mock(OpcodeStack.class);
+
+		Item itemInStack = mock(Item.class);
+		doReturn(itemInStack).when(stack).getStackItem(0);
+
+		assertThat(detector.countParameter(stack, "(Ljava/lang/String;Ljava/lang/Object;)V"), is(1));
+		assertThat(detector.countParameter(stack, "(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"), is(2));
+		assertThat(detector.countParameter(stack, "(Lorg/slf4j/Maker;Ljava/lang/String;Ljava/lang/Object;)V"), is(1));
+		assertThat(detector.countParameter(stack, "(Lorg/slf4j/Maker;Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"), is(2));
+
+		Item exceptionInStack = mock(Item.class);
+		doReturn("IS_THROWABLE").when(exceptionInStack).getUserValue();
+		doReturn(exceptionInStack).when(stack).getStackItem(0);
+
+		assertThat(detector.countParameter(stack, "(Ljava/lang/String;Ljava/lang/Object;)V"), is(0));
+		assertThat(detector.countParameter(stack, "(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"), is(1));
+		assertThat(detector.countParameter(stack, "(Lorg/slf4j/Maker;Ljava/lang/String;Ljava/lang/Object;)V"), is(0));
+		assertThat(detector.countParameter(stack, "(Lorg/slf4j/Maker;Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"), is(1));
 	}
 }
