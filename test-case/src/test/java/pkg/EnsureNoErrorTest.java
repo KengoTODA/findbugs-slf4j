45b3a25b29ba713015384b60fd7c356b4e4ffaf2
eller86 <skypencil@gmail.com>
2012-10-22 23:49:20 +0800

    added test caase to check errors.

---
+++ test-case/src/test/java/pkg/EnsureNoErrorTest.java
@@ -0,0 +1,32 @@
+package pkg;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class EnsureNoErrorTest {
+
+	@Test
+	public void test() throws SAXException, IOException, ParserConfigurationException {
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		Document doc = docBuilder.parse("target/findbugsXml.xml");
+		NodeList errorsList = doc.getElementsByTagName("Errors");
+		Node errorsNode = errorsList.item(0);
+		String errorCount = errorsNode.getAttributes().getNamedItem("errors").getNodeValue();
+
+		assertThat(errorCount, is("0"));
+	}
+
+}
