c9d312e67a8961c8bb6452fdbbb5f826674a943b
eller86 <skypencil@gmail.com>
2012-10-28 17:42:52 +0800

    add test case to reproduce a bug. now build is RED.

---
+++ test-case/src/main/java/pkg/UsingReturnedExceptionToLog.java
@@ -0,0 +1,43 @@
+package pkg;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class UsingReturnedExceptionToLog {
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	void methodWithClass() {
+		Utility utility = new Utility();
+		logger.warn("log exception", utility.utility());
+		LoggerFactory.getLogger(getClass()).warn("log exception", utility.utility());
+
+		logger.warn("something {}", "happen", utility.utility());
+		LoggerFactory.getLogger(getClass()).warn("something {}", "happen", utility.utility());
+
+		logger.warn("{} {}", new Object[]{ "something", "happen", utility.utility() });
+		LoggerFactory.getLogger(getClass()).warn("{} {}", new Object[]{ "something", "happen", utility.utility() });
+	}
+
+	void methodWithInterface() {
+		UtilityInterface utility = new Utility();
+		logger.warn("log exception", utility.utility());
+		LoggerFactory.getLogger(getClass()).warn("log exception", utility.utility());
+
+		logger.warn("something {}", "happen", utility.utility());
+		LoggerFactory.getLogger(getClass()).warn("something {}", "happen", utility.utility());
+
+		logger.warn("{} {}", new Object[]{ "something", "happen", utility.utility() });
+		LoggerFactory.getLogger(getClass()).warn("{} {}", new Object[]{ "something", "happen", utility.utility() });
+	}
+
+	private static interface UtilityInterface {
+		Throwable utility();
+	}
+
+	private class Utility implements UtilityInterface {
+		@Override
+		public Throwable utility() {
+			return new RuntimeException();
+		}
+	}
+}
