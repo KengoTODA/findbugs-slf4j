f7183770b9e554af0d35056c3270d37926a71cf1
eller86 <skypencil@gmail.com>
2012-11-11 21:12:37 +0800

    add test case to start implementing new bug detector.

---
+++ test-case/src/main/java/pkg/PublishLogger.java
@@ -0,0 +1,12 @@
+package pkg;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PublishLogger {
+	public Logger logger = LoggerFactory.getLogger(getClass());
+	void method() {
+		logger.info("using log message");
+		LoggerFactory.getLogger(getClass()).info("using log message");
+	}
+}
