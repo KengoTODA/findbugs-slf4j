57842ef44a4ca6bc447a3bcdb2aacee8e32c22c9
eller86 <skypencil@gmail.com>
2012-11-25 22:57:18 +0800

    add 2 corner cases

---
+++ test-case/src/main/java/pkg/UsingInnerClass.java
@@ -0,0 +1,16 @@
+package pkg;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UsingInnerClass {
+    static final class InnerClass {
+    }
+
+    private final Logger logger = LoggerFactory.getLogger(InnerClass.class);
+
+    void method() {
+        logger.info("hello, world");
+        LoggerFactory.getLogger(InnerClass.class).info("Hello, world");
+    }
+}
