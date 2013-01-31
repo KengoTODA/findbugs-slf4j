df2af87b47bb59f9088bfbbbbeeba786e84b4aa6
Kengo TODA <skypencil@gmail.com>
2013-02-01 07:48:56 +0800

    create new API which uses Map to specify bug counts.

---
+++ test-case/src/test/groovy/jp/skypencil/findbugs/slf4j/Issue2Test.groovy
@@ -0,0 +1,12 @@
+package jp.skypencil.findbugs.slf4j
+
+import org.junit.Test
+
+class Issue2Test {
+  @Test
+  public void testToFindPlaceHolderMismatch() {
+    new XmlParser().expect(pkg.Issue2, [
+      'SLF4J_PLACE_HOLDER_MISMATCH': 1
+    ]);
+  }
+}
--- test-case/src/test/java/jp/skypencil/findbugs/slf4j/XmlParser.java
+++ test-case/src/test/java/jp/skypencil/findbugs/slf4j/XmlParser.java
@@ -1,10 +1,13 @@
 package jp.skypencil.findbugs.slf4j;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -17,6 +20,55 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 class XmlParser {
+    void expect(Class<?> clazz, Map<String, Integer> expected) {
+        final String filePath = "pkg." + clazz.getSimpleName();
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory
+                    .newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            Document doc = docBuilder.parse("target/findbugsXml.xml");
+            NodeList bugs = doc.getElementsByTagName("BugInstance");
+            Map<String, Integer> actual = new HashMap<String, Integer>();
+
+            for (int i = 0; i < bugs.getLength(); ++i) {
+                Node bug = bugs.item(i);
+                NamedNodeMap attr = bug.getAttributes();
+                String className = findClass(bug).getNamedItem("classname").getNodeValue();
+                if (className.equals(filePath)) {
+                    String bugType = attr.getNamedItem("type").getNodeValue();
+                    Integer counter = actual.get(bugType);
+                    if (counter == null) {
+                        actual.put(bugType, 1);
+                    } else {
+                        actual.put(bugType, counter + 1);
+                    }
+                }
+            }
+            assertThat(actual, is(equalTo(expected)));
+        } catch (ParserConfigurationException e) {
+            throw new AssertionError(e);
+        } catch (SAXException e) {
+            throw new AssertionError(e);
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    private NamedNodeMap findClass(Node bug) {
+        NodeList children = bug.getChildNodes();
+        for (int i = 0; i < children.getLength(); ++i) {
+            Node child = children.item(i);
+            if (child.getNodeName().equals("Class")) {
+                return child.getAttributes();
+            }
+        }
+        throw new AssertionError();
+    }
+
+    /**
+     * @deprecated use {@code #expect(Class, Map)} instead.
+     */
+    @Deprecated
     void expectBugs(Class<?> clazz, int expectedBugs) {
         final String className = clazz.getSimpleName();
         final String filePath = "pkg/" + className + ".java";
