a23e0970c52c027b9a533663edee65425c3a13fc
Kengo TODA <skypencil@gmail.com>
2013-02-01 07:11:11 +0800

    replace TAB with spaces

--- test-case/src/test/java/jp/skypencil/findbugs/slf4j/XmlParser.java
+++ test-case/src/test/java/jp/skypencil/findbugs/slf4j/XmlParser.java
@@ -17,41 +17,44 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 class XmlParser {
+    void expectBugs(Class<?> clazz, int expectedBugs) {
+        final String className = clazz.getSimpleName();
+        final String filePath = "pkg/" + className + ".java";
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory
+                    .newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            Document doc = docBuilder.parse("target/findbugsXml.xml");
+            NodeList fileStats = doc.getElementsByTagName("FileStats");
 
-	void expectBugs(Class<?> clazz, int expectedBugs) {
-		final String className = clazz.getSimpleName();
-		final String filePath = "pkg/" + className + ".java";
-		try {
-			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-			Document doc = docBuilder.parse("target/findbugsXml.xml");
-			NodeList fileStats = doc.getElementsByTagName("FileStats");
+            for (int i = 0; i < fileStats.getLength(); ++i) {
+                Node node = fileStats.item(i);
+                NamedNodeMap attr = node.getAttributes();
+                if (attr.getNamedItem("path").getNodeValue().equals(filePath)) {
+                    final String bugCount = attr.getNamedItem("bugCount")
+                            .getNodeValue();
+                    final String message;
+                    if (expectedBugs == 0) {
+                        message = filePath + " should have no bug";
+                    } else {
+                        message = filePath + " should have " + expectedBugs
+                                + " bugs";
+                    }
 
-			for (int i = 0; i < fileStats.getLength(); ++i) {
-				Node node = fileStats.item(i);
-				NamedNodeMap attr = node.getAttributes();
-				if (attr.getNamedItem("path").getNodeValue().equals(filePath)) {
-					final String bugCount = attr.getNamedItem("bugCount").getNodeValue();
-					final String message;
-					if (expectedBugs == 0) {
-						message = filePath + " should have no bug";
-					} else {
-						message = filePath + " should have " + expectedBugs + " bugs";
-					}
+                    assertThat(message, bugCount,
+                            is(Integer.toString(expectedBugs)));
+                    return;
+                }
+            }
 
-					assertThat(message, bugCount, is(Integer.toString(expectedBugs)));
-					return;
-				}
-			}
-
-			fail("Result of " + filePath + " not found");
-		} catch (ParserConfigurationException e) {
-			throw new AssertionError(e);
-		} catch (SAXException e) {
-			throw new AssertionError(e);
-		} catch (IOException e) {
-			throw new AssertionError(e);
-		}
-	}
+            fail("Result of " + filePath + " not found");
+        } catch (ParserConfigurationException e) {
+            throw new AssertionError(e);
+        } catch (SAXException e) {
+            throw new AssertionError(e);
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+    }
 
 }
