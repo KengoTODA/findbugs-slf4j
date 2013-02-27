df0968f1accdf963aa82bc7aaa41222733ed49e6
Kengo TODA <skypencil@gmail.com>
2013-02-27 23:19:03 +0800

    added several test cases. refs #12

--- test-case/src/main/java/pkg/UsingArrayAsArgument.java
+++ test-case/src/main/java/pkg/UsingArrayAsArgument.java
@@ -5,7 +5,11 @@ import org.slf4j.LoggerFactory;
 
 public final class UsingArrayAsArgument {
     private final Logger logger = LoggerFactory.getLogger(getClass());
-    void method(Object[] args) {
+    void reportedPattern(Object[] args) {
         logger.info("Logging int array {}.", int[].class);
+    }
+    void additionalPattern(Object[] args) {
+        logger.info("Logging int[] array {}.", int[][].class);
+        logger.info("Logging Object array {}.", Object[].class);
     }
 }
