0bf7f02bbe7508b3c09e25e672cd7f6014b0b793
Kengo TODA <skypencil@gmail.com>
2013-03-05 22:32:12 +0800

    reproduce reported bugs. refs #14 and #15

---
+++ test-case/src/main/java/pkg/UsingMarker.java
@@ -0,0 +1,20 @@
+package pkg;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+
+/**
+ * Simple class to reproduce issue 15.
+ *
+ * @see https://github.com/eller86/findbugs-slf4j/issues/15
+ */
+public class UsingMarker {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    void method() {
+        Marker marker = null;
+        logger.error(marker, "Hello, marker");
+        logger.error(marker, "Hello, {}", "world");
+    }
+}
