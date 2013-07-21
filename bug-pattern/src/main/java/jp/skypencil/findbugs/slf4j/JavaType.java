3c255d4b4f1864bbd1834f23bc1aa82a362634e0
Kengo TODA <skypencil@gmail.com>
2013-07-21 14:10:05 +0800

    refactor: collect dependency on Repository.

--- bug-pattern/src/main/java/jp/skypencil/findbugs/slf4j/JavaType.java
+++ bug-pattern/src/main/java/jp/skypencil/findbugs/slf4j/JavaType.java
@@ -1,14 +1,18 @@
 package jp.skypencil.findbugs.slf4j;
 import static org.apache.bcel.Repository.lookupClass;
 
+import org.apache.bcel.Repository;
 import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.generic.BasicType;
 import org.apache.bcel.generic.Type;
 
 import com.google.common.collect.ImmutableSet;
 
+import edu.umd.cs.findbugs.internalAnnotations.SlashedClassName;
+
 public final class JavaType {
     private static final ImmutableSet<BasicType> PRIMITIVE_TYPES;
+    private static final JavaClass THROWABLE;
     private final String name;  // "java.lang.String" etc.
     private final JavaClass javaClass;
 
@@ -16,6 +20,21 @@ public final class JavaType {
         PRIMITIVE_TYPES = ImmutableSet.of(
                 Type.BOOLEAN, Type.BYTE, Type.SHORT, Type.CHAR, Type.INT,
                 Type.LONG, Type.FLOAT, Type.DOUBLE);
+        try {
+            THROWABLE = Repository.lookupClass(Throwable.class);
+        } catch (ClassNotFoundException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    public static JavaType from(@SlashedClassName String className) {
+        JavaClass javaClass;
+        try {
+            javaClass = Repository.lookupClass(className);
+            return from(javaClass);
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     public static JavaType from(JavaClass clazz) {
@@ -56,6 +75,18 @@ public final class JavaType {
             return null;
         } else {
             return lookupClass(name);
+        }
+    }
+
+    public boolean isThrowable() {
+        try {
+            JavaClass javaClass = toJavaClass();
+            if (javaClass == null) {
+                return false;
+            }
+            return javaClass.instanceOf(THROWABLE);
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException(e);
         }
     }
 }
--- bug-pattern/src/main/java/jp/skypencil/findbugs/slf4j/parameter/ThrowableHandler.java
+++ bug-pattern/src/main/java/jp/skypencil/findbugs/slf4j/parameter/ThrowableHandler.java
@@ -1,10 +1,8 @@
 package jp.skypencil.findbugs.slf4j.parameter;
 
-import static org.apache.bcel.Repository.lookupClass;
 import jp.skypencil.findbugs.slf4j.JavaType;
 
 import org.apache.bcel.Constants;
-import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.generic.Type;
 
 import edu.umd.cs.findbugs.OpcodeStack;
@@ -14,15 +12,6 @@ import edu.umd.cs.findbugs.classfile.FieldDescriptor;
 
 public class ThrowableHandler {
     private static final String IS_THROWABLE = "IS_THROWABLE";
-    private final JavaClass throwable;
-
-    public ThrowableHandler() {
-        try {
-            this.throwable = lookupClass("java/lang/Throwable");
-        } catch (ClassNotFoundException e) {
-            throw new AssertionError(e);
-        }
-    }
 
     void sawOpcode(OpcodeStackDetector detector, int seen) {
         if (detector.atCatchBlock()) {
@@ -35,59 +24,48 @@ public class ThrowableHandler {
      * Call this method after #afterOpcode of detector.
      */
     void afterOpcode(OpcodeStackDetector detector, int seen) {
-        try {
-            OpcodeStack stack = detector.getStack();
-            if (seen == Constants.NEW && lookupClass(detector.getClassConstantOperand()).instanceOf(throwable)) {
-                Item createdThrowable = stack.getStackItem(0);
-                createdThrowable.setUserValue(IS_THROWABLE);
-            } else if (seen == Constants.INVOKEINTERFACE || seen == Constants.INVOKEVIRTUAL || seen == Constants.INVOKESPECIAL || seen == Constants.INVOKESTATIC) {
-                String signature = detector.getMethodDescriptorOperand().getSignature();
-                String returnType = signature.substring(1 + signature.lastIndexOf(')'));
-                if (!returnType.startsWith("L")) {
-                    // primitive value should be non-throwable
-                    return;
-                }
+        OpcodeStack stack = detector.getStack();
+        if (seen == Constants.NEW && JavaType.from(detector.getClassConstantOperand()).isThrowable()) {
+            Item createdThrowable = stack.getStackItem(0);
+            createdThrowable.setUserValue(IS_THROWABLE);
+        } else if (seen == Constants.INVOKEINTERFACE || seen == Constants.INVOKEVIRTUAL || seen == Constants.INVOKESPECIAL || seen == Constants.INVOKESTATIC) {
+            String signature = detector.getMethodDescriptorOperand().getSignature();
+            String returnType = signature.substring(1 + signature.lastIndexOf(')'));
+            if (!returnType.startsWith("L")) {
+                // primitive value should be non-throwable
+                return;
+            }
 
-                returnType = returnType.substring(1, returnType.length() - 1).replaceAll("/", ".");
-                JavaClass returnClass = lookupClass(returnType);
-                if (returnClass.instanceOf(throwable)) {
-                    Item returnedThrowable = stack.getStackItem(0);
-                    returnedThrowable.setUserValue(IS_THROWABLE);
-                }
-            } else if (seen == Constants.ALOAD || seen == Constants.ALOAD_0 || seen == Constants.ALOAD_1 || seen == Constants.ALOAD_2 || seen == Constants.ALOAD_3) {
-                final int index;
-                switch (seen) {
-                case Constants.ALOAD_0: index = 0; break;
-                case Constants.ALOAD_1: index = 1; break;
-                case Constants.ALOAD_2: index = 2; break;
-                case Constants.ALOAD_3: index = 3; break;
-                default:
-                    index = detector.getIntConstant();
-                }
-                JavaType typeOfLocalVar = loadLocalVar(detector, index);
-                if (typeOfLocalVar != null) {
-                    JavaClass javaClass = typeOfLocalVar.toJavaClass();
-                    if (javaClass != null && javaClass.instanceOf(throwable)) {
-                        Item localThrowable = stack.getStackItem(0);
-                        localThrowable.setUserValue(IS_THROWABLE);
-                    }
-                }
-            } else if (seen == Constants.GETFIELD || seen == Constants.GETSTATIC) {
-                FieldDescriptor targetField = detector.getFieldDescriptorOperand();
-                Type type = Type.getType(targetField.getSignature());
-                if (type != null) {
-                    JavaType javaType = JavaType.from(type);
-                    if (javaType != null) {
-                        JavaClass javaClass = javaType.toJavaClass();
-                        if (javaClass != null && javaClass.instanceOf(throwable)) {
-                            Item localThrowable = stack.getStackItem(0);
-                            localThrowable.setUserValue(IS_THROWABLE);
-                        }
-                    }
+            returnType = returnType.substring(1, returnType.length() - 1).replaceAll("/", ".");
+            if (JavaType.from(returnType).isThrowable()) {
+                Item returnedThrowable = stack.getStackItem(0);
+                returnedThrowable.setUserValue(IS_THROWABLE);
+            }
+        } else if (seen == Constants.ALOAD || seen == Constants.ALOAD_0 || seen == Constants.ALOAD_1 || seen == Constants.ALOAD_2 || seen == Constants.ALOAD_3) {
+            final int index;
+            switch (seen) {
+            case Constants.ALOAD_0: index = 0; break;
+            case Constants.ALOAD_1: index = 1; break;
+            case Constants.ALOAD_2: index = 2; break;
+            case Constants.ALOAD_3: index = 3; break;
+            default:
+                index = detector.getIntConstant();
+            }
+            JavaType typeOfLocalVar = loadLocalVar(detector, index);
+            if (typeOfLocalVar != null && typeOfLocalVar.isThrowable()) {
+                Item localThrowable = stack.getStackItem(0);
+                localThrowable.setUserValue(IS_THROWABLE);
+            }
+        } else if (seen == Constants.GETFIELD || seen == Constants.GETSTATIC) {
+            FieldDescriptor targetField = detector.getFieldDescriptorOperand();
+            Type type = Type.getType(targetField.getSignature());
+            if (type != null) {
+                JavaType javaType = JavaType.from(type);
+                if (javaType != null && javaType.isThrowable()) {
+                    Item localThrowable = stack.getStackItem(0);
+                    localThrowable.setUserValue(IS_THROWABLE);
                 }
             }
-        } catch (ClassNotFoundException e) {
-            throw new IllegalStateException("class not found", e);
         }
     }
 
