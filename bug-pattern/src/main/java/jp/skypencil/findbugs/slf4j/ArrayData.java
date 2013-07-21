d3aa409b262ae01b55c8a7d63e181d0f3dfcdc8f
Kengo TODA <skypencil@gmail.com>
2013-07-21 11:45:06 +0800

    replace tabs with spaces.

--- bug-pattern/src/main/java/jp/skypencil/findbugs/slf4j/ArrayData.java
+++ bug-pattern/src/main/java/jp/skypencil/findbugs/slf4j/ArrayData.java
@@ -1,22 +1,22 @@
 package jp.skypencil.findbugs.slf4j;
 
 class ArrayData {
-	private final int size;
-	private boolean hasThrowableAtLast;
+    private final int size;
+    private boolean hasThrowableAtLast;
 
-	ArrayData(int size) {
-		this.size = size;
-	}
+    ArrayData(int size) {
+        this.size = size;
+    }
 
-	int getSize() {
-		return size;
-	}
+    int getSize() {
+        return size;
+    }
 
-	void setThrowableAtLast(boolean hasThrowableAtLast) {
-		this.hasThrowableAtLast = hasThrowableAtLast;
-	}
+    void setThrowableAtLast(boolean hasThrowableAtLast) {
+        this.hasThrowableAtLast = hasThrowableAtLast;
+    }
 
-	boolean hasThrowableAtLast() {
-		return hasThrowableAtLast;
-	}
+    boolean hasThrowableAtLast() {
+        return hasThrowableAtLast;
+    }
 }
