a3c696c21cad865bb6ae1603c0533c01ba5d707b
eller86 <skypencil@gmail.com>
2013-10-06 20:49:04 +0800

    refs #14: reproduce reported bug by @theJC

--- test-case/src/main/java/pkg/Issue14.java
+++ test-case/src/main/java/pkg/Issue14.java
@@ -1,5 +1,8 @@
 package pkg;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,20 +12,21 @@ import org.slf4j.LoggerFactory;
  * @see https://github.com/eller86/findbugs-slf4j/issues/14
  */
 public class Issue14 {
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-
-    void method(String requestId) {
-        try {
-            throw new RuntimeException();
-        } catch (RuntimeException e) {
-            if (e instanceof IllegalStateException
-                    || e instanceof IllegalArgumentException
-                    || e.getCause() instanceof IllegalStateException) {
-                logger.error("Failed on requestId={} due to an exception ",
-                        requestId, e);
-            } else {
-                throw e;
+    private final Logger LOGGER = LoggerFactory.getLogger(Issue14.class);
+    public List<Long> handle(List<Long> requests) {
+        List<Long> result = new ArrayList<Long>();
+        result.add(1L);
+        for (Long request : requests) {
+             try {
+                result.add(request);
+            } catch (RuntimeException e) {
+                if (e instanceof ClassCastException || e.getCause() instanceof NullPointerException) {
+                    LOGGER.error("Failed to process request={}, moving to next ", request.toString(), e);
+                } else {
+                    throw e;
+                }
             }
         }
+        return result;
     }
 }
