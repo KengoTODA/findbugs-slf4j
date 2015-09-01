1b449866b1235b9fcce162b1381d7374f71ef11b
Kengo TODA <skypencil@gmail.com>
2015-09-01 23:06:27 +0800

    refs #35: reproduce problem

--- test-case/src/main/java/pkg/Issue35.java
+++ test-case/src/main/java/pkg/Issue35.java
@@ -4,28 +4,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * @see https://github.com/KengoTODA/findbugs-slf4j/issues/35#issuecomment-135973699
+ * @see https://github.com/KengoTODA/findbugs-slf4j/issues/35
  */
 public class Issue35 {
-    private static final String MSG = "{}: abcd={}"
-            + ", really long text forcing line wraps for formatting={}"
-            + ", y={}";
+    private static final String MSG = "CONSTANT VALUE";
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
-    void logMessage(final String a, final String b, final MyClass c) {
-        final Y y = c.getX().getY();
-        log.error(MSG, new Object[]{"literal", a, b, y});
+    public void message() {
+        logMessage(MSG);
     }
 
-    private static interface X {
-        Y getY();
-    }
-
-    private static interface Y {
-    }
-
-    private static abstract class MyClass {
-        abstract X getX();
+    private void logMessage(String messageFromParameter) {
+        log.info(messageFromParameter);
     }
 }
