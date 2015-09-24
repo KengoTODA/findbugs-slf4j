dc2738e912d3466cae1b9b58d67fa7e09e185034
Kengo TODA <skypencil@gmail.com>
2015-09-25 07:01:56 +0800

    refs #39: add test case for UsingArgumentAsParameter.java

--- test-case/src/test/java/jp/skypencil/findbugs/slf4j/UsingArgumentAsParameterTest.java
+++ test-case/src/test/java/jp/skypencil/findbugs/slf4j/UsingArgumentAsParameterTest.java
@@ -1,14 +1,20 @@
 package jp.skypencil.findbugs.slf4j;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import java.util.Collections;
 import java.util.Map;
 
 import org.junit.Test;
 
+import com.google.common.collect.Multimap;
+
 public class UsingArgumentAsParameterTest {
   @Test
   public void test() {
     Map<String, Integer> expected = Collections.singletonMap("SLF4J_UNKNOWN_ARRAY", 2);
-    new XmlParser().expect(pkg.UsingArgumentAsParameter.class, expected);
+    Multimap<String, String> longMessages = new XmlParser().expect(pkg.UsingArgumentAsParameter.class, expected);
+    assertThat(longMessages).containsEntry("SLF4J_UNKNOWN_ARRAY",
+            "Using unknown array as parameter. You cannot use array which is provided as method argument or returned from other method.");
   }
 }
