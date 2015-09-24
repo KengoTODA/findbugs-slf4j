68c01f09eef105c2fc998bd02c4cac6265f57da7
Kengo TODA <skypencil@gmail.com>
2015-09-24 08:51:26 +0800

    refs #39: add test case for Issue2.java

--- test-case/src/test/java/jp/skypencil/findbugs/slf4j/Issue2Test.java
+++ test-case/src/test/java/jp/skypencil/findbugs/slf4j/Issue2Test.java
@@ -1,14 +1,20 @@
 package jp.skypencil.findbugs.slf4j;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import java.util.Collections;
 import java.util.Map;
 
 import org.junit.Test;
 
+import com.google.common.collect.Multimap;
+
 public class Issue2Test {
   @Test
   public void testToFindPlaceHolderMismatch() {
       Map<String, Integer> expectedError = Collections.singletonMap("SLF4J_PLACE_HOLDER_MISMATCH", 1);
-    new XmlParser().expect(pkg.Issue2.class, expectedError);
+    Multimap<String, String> longMessages = new XmlParser().expect(pkg.Issue2.class, expectedError);
+    assertThat(longMessages)
+      .containsEntry("SLF4J_PLACE_HOLDER_MISMATCH", "Count of placeholder(4) is not equal to count of parameter(1)");
   }
 }
