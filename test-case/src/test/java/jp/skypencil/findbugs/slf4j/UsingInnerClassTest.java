59c1ada34dac7c62a18f40eb8b0a2f5c22307564
Kengo TODA <skypencil@gmail.com>
2015-09-25 07:40:10 +0800

    refs #39: add test case for UsingInnerClassTest.java

--- test-case/src/test/java/jp/skypencil/findbugs/slf4j/UsingInnerClassTest.java
+++ test-case/src/test/java/jp/skypencil/findbugs/slf4j/UsingInnerClassTest.java
@@ -1,14 +1,20 @@
 package jp.skypencil.findbugs.slf4j;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import java.util.Collections;
 import java.util.Map;
 
 import org.junit.Test;
 
+import com.google.common.collect.Multimap;
+
 public class UsingInnerClassTest {
   @Test
   public void test() {
     Map<String, Integer> expected = Collections.singletonMap("SLF4J_ILLEGAL_PASSED_CLASS", 2);
-    new XmlParser().expect(pkg.UsingInnerClass.class, expected);
+    Multimap<String, String> longMessages = new XmlParser().expect(pkg.UsingInnerClass.class, expected);
+    assertThat(longMessages).containsEntry("SLF4J_ILLEGAL_PASSED_CLASS",
+            "Illegal class is passed to LoggerFactory.getLogger(Class). It should be one of [pkg.UsingInnerClass].");
   }
 }
