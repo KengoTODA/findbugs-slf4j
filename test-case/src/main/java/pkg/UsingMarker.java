a948eb0100e9b647fc811c5054b3c04c36703b53
Kengo TODA <skypencil@gmail.com>
2018-10-10 08:08:45 +0900

    stop using null as marker

--- test-case/src/main/java/pkg/UsingMarker.java
+++ test-case/src/main/java/pkg/UsingMarker.java
@@ -3,6 +3,7 @@ package pkg;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 
 /**
  * Simple class to reproduce issue 15.
@@ -13,7 +14,7 @@ public class UsingMarker {
   private final Logger logger = LoggerFactory.getLogger(getClass());
 
   void method() {
-    Marker marker = null;
+    Marker marker = MarkerFactory.getMarker("my marker");
     logger.error(marker, "Hello, marker");
     logger.error(marker, "Hello, {}");
     logger.error(marker, "Hello, {}", "world");
