a231d489fe19f65904c02b03d7b6e8bbc66543e5
Kengo TODA <skypencil@gmail.com>
2022-05-11 13:49:07 +0800

    test: update the test to support the latest SpotBugs

--- test-case/src/test/java/jp/skypencil/findbugs/slf4j/Issue14Test.java
+++ test-case/src/test/java/jp/skypencil/findbugs/slf4j/Issue14Test.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class Issue14Test {
   @Test
   public void testToFindPlaceHolderMismatch() {
-    Map<String, Integer> expected = Collections.emptyMap();
+    Map<String, Integer> expected = Collections.singletonMap("THROWS_METHOD_THROWS_RUNTIMEEXCEPTION", 1);
     new XmlParser().expect(pkg.Issue14.class, expected);
   }
 }
